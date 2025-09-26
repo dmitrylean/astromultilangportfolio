d6af04f3b1be9ae0f5ae7340b234fd10eeeedd2d
dmitrylean <adam.lean.92@gmail.com>
2025-09-26 17:17:26 +0800

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,13 +6,12 @@ const blog = defineCollection({
     date: z.coerce.date(),
     description: z.string().optional(),
     image: z.string().optional(),
-    // если в старых постах встречается строка, временно разреши union:
     categories: z.union([z.array(z.string()), z.string()]).optional(),
   }),
 });
 
 const works = defineCollection({
-  type: "data", // или "content", зависит от того, что у тебя в папке
+  type: "data",
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
